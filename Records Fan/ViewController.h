8e2f79080c520774091452f127e5fcc3b71de1e1
Qu Chong <quchong@QutekiMac-mini.local>
2014-11-24 20:59:37 +0800

    Initial Commit

---
+++ Records Fan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Records Fan
+//
+//  Created by Qu Chong on 14/11/24.
+//  Copyright (c) 2014年 Qu Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
